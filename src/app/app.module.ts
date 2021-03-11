d2a0a71cf3f5db6d378d8a73712d3815221056a1
Tagor Petrosian <tagorp@top4.com>
2021-03-11 13:37:33 +0200

    styles overriding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccordionComponent } from './accordion/accordion.component';
-import { AccordionItemComponent } from './accordion/accordion-item/accordion-item.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
-    declarations: [AppComponent, AccordionComponent, AccordionItemComponent],
+    declarations: [AppComponent, AccordionComponent],
     imports: [BrowserModule, BrowserAnimationsModule, MatExpansionModule],
     providers: [],
     bootstrap: [AppComponent],
